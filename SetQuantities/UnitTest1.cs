352694d3d3025ecb9c910e5f84ecd0674a9181cc
software <software@DESKTOP-8DED6EV>
2022-06-24 17:27:50 +0300

    unit test

---
+++ SetQuantities/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SetQuantities
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/UnitTests/ApplicationCore/Services/BasketServiceTests/SetQuantities.cs
@@ -0,0 +1,36 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Services;
+using ApplicationCore.Exceptions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.ApplicationCore.Services.BasketServiceTests
+{
+    public class SetQuantities
+    {
+        private readonly string sampleBuyerId = Guid.NewGuid().ToString();
+        private readonly Mock<IRepository<Basket>> _mockBasketRepo = new Mock<IRepository<Basket>>();
+        [Fact]
+        public async Task ThrowsGivenNonpositiveQuantities()
+        {
+            var basket = new BasketService(_mockBasketRepo.Object);
+
+            var quantities = new Dictionary<int, int>()
+            {
+                { 3, 4 },
+                { 2, 0 },
+                { 5, -8 }
+            };
+            await Assert.ThrowsAsync<NonpositiveQuantityException>(async () =>
+            {
+                await basket.SetQuantities(sampleBuyerId, quantities);
+            });
+        }
+    }
+}
